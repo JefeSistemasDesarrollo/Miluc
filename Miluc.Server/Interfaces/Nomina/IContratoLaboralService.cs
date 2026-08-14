752775658c62f1e0ea46073e3c4b0f50b8f5ea7e
ypirajan19-art <ypirajan19@gmail.com>
2026-08-14 10:43:00 -0500

    #correcion de contratolaboral

--- Miluc.Server/Interfaces/Nomina/IContratoLaboralService.cs
+++ Miluc.Server/Interfaces/Nomina/IContratoLaboralService.cs
@@ -6,7 +6,7 @@ namespace Miluc.Server.Interfaces.Nomina
     public interface IContratoLaboralService 
 
     {
-        public Task<(List<ContratoLabralreaderDto>data, int CantidadDeRegistros)> GetContratoLaboralAsync(string? filtro = null, int page = 1, int? cantidad = null);
+        public Task<(List<ContratoLabralreaderDto>data, int CantidadDeRegistros)> GetContratoLaboralAsync(string? filtro = null, int ? page = null, int? cantidad = null);
 
        public Task<ContratoLabralreaderDto> GetContratoById(int idcontrato);
         public Task<bool> UpsertContratoLaboralAsync(UpdateContratoLaboralDto dto);
--- Miluc.Server/Servicios/Nomina/ContratoLaboralService.cs
+++ Miluc.Server/Servicios/Nomina/ContratoLaboralService.cs
@@ -1,9 +1,6 @@
-﻿using Microsoft.AspNetCore.Components.Forms;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Miluc.Server.Data;
 using Miluc.Server.Interfaces.Nomina;
-using Miluc.Server.Models.Nomina;
-using Miluc.Shared.DTOs.Nomina.ContratoLaboralDetalleDto;
 
 using Miluc.Shared.DTOs.Nomina.ContratoLaboralDto;
 
@@ -15,69 +12,12 @@ namespace Miluc.Server.Servicios.Nomina
         {
             throw new NotImplementedException();
         }
-
-
-
-
-        //public async Task<ContratoLabralreaderDto> GetContratoById(int idcontrato)
-        //{
-        //    try
-        //    {
-        //        var contratoLabId = await _context.ContratoLaboral.AsNoTracking()
-        //        .Include(t => t.TipoContrato)
-        //        .Include(e => e.Empresa)
-        //        .Include(e => e.Empleado)
-        //        .Include(c => c.ContratoLaboralDetalle)
-        //        .Where(c => c.ContratoLaboralId == idcontrato)
-        //        .Select(c => new ContratoLabralreaderDto
-        //        {
-
-
-        //            ContratoLaboralId = c.ContratoLaboralId,
-        //            EmpresaId = c.EmpresaId,
-        //            NombreTipoContrato = c.TipoContrato.NombreContrato,
-        //            //ContratoLaboralDetalleId = c.ContratoLaboralDetalleId,
-        //            FechaCreacion = c.FechaCreacion,
-        //            //RELACION CON EL NOMBRE DE LA EMPRESA
-        //            NombreEmpresa = c.Empresa.NombreEmpresa,
-        //            //RELACION CON EL EMPLEADO
-        //            EmpleadoId = c.EmpleadoId,
-        //            //  Relacion Concatenar  los nombres del empleado para mostrar el nombre completo
-        //            NombreEmpleado = $"{c.Empleado.PrimerNombre} {c.Empleado.SegundoNombre} {c.Empleado.PrimerApellido} {c.Empleado.SegundoApellido}",
-        //            //Relacion con el nombre del tipo de contrato
-        //            TipoContratoId = c.TipoContratoId,
-        //            //ContratoLaboralDetalleId = c.ContratoLaboralDetalle.Select(c => new DetalleContratoLaboralDto
-        //            // {
-        //            //    ContratoLaboralDetalleId = c.ContratoLaboralDetalleId,
-        //            //     FechaInicio = c.FechaInicio,
-        //            //     FechaFinalizacion = c.FechaFinalizacion,
-        //            //     Cargo = c.Cargo,
-        //            //     CentroCosto = c.CentroCosto,
-        //            //     Salario = c.Salario,
-        //            // }).ToList()
-
-
-        //        }).FirstOrDefaultAsync();
-
-        //        if (contratoLabId == null)
-        //            throw new Exception($"error al buscar contrato labolar{idcontrato} ");
-
-
-        //        return contratoLabId;
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        throw new Exception($"Error al obtener los contratos laborales: {ex.Message}");
-        //    }
-        //}
-
-
-        public async Task<(List<ContratoLabralreaderDto> data, int CantidadDeRegistros)> GetContratoLaboralAsync(string? filtro = null, int page = 1, int? cantidad = null)
+        public async Task<(List<ContratoLabralreaderDto> data, int CantidadDeRegistros)> GetContratoLaboralAsync(string? filtro = null, int ? page =null, int? cantidad = null)
         {
             try
             {
                 int cantidadtop = cantidad ?? 20;
-
+                int paginador = page ?? 1;
                 var query = _context.Empleado.AsNoTracking().AsQueryable();
 
                 if (!string.IsNullOrEmpty(filtro))
@@ -91,7 +31,7 @@ namespace Miluc.Server.Servicios.Nomina
                 var totalRegistros = await query.CountAsync();
 
                 var datosIntermedios = await query
-                    .Skip((page - 1) * cantidadtop)
+                    .Skip((paginador - 1) * cantidadtop)
                     .Take(cantidadtop)
                     .Select(x => new
                     {
@@ -167,93 +107,14 @@ namespace Miluc.Server.Servicios.Nomina
                 throw new Exception($"Error al obtener contratos laborales: {ex.Message}", ex);
             }
         }
-        
+
         public Task<bool> UpsertContratoLaboralAsync(UpdateContratoLaboralDto dto)
         {
             throw new NotImplementedException();
         }
-        //public async Task<bool> UpsertContratoLaboralAsync(UpdateContratoLaboralDto dto)
-        //{
-        //    try
-        //    {
-        //        // 1. Buscar si ya existe el contrato cargando sus detalles asociados
-        //        var contratoExistente = await _context.ContratoLaboral
-        //            .Include(c => c.ContratoLaboralDetalle)
-        //            .FirstOrDefaultAsync(c => (dto.ContratoLaboralId > 0 && c.ContratoLaboralId == dto.ContratoLaboralId)
-        //                                   || (c.EmpleadoId == dto.EmpleadoId));
-
-        //        if (contratoExistente == null)
-        //        {
-
-        //            var nuevoContrato = new ContratoLaboral
-        //            {
-        //                EmpleadoId = dto.EmpleadoId,
-        //                EmpresaId = dto.EmpresaId,
-        //                TipoContratoId = dto.TipoContratoId,
-        //                FechaCreacion = DateTime.Now,
-        //                ContratoLaboralDetalle = new List<ContratoLaboralDetalle>
-        //        {
-        //            new ContratoLaboralDetalle
-        //            {
-        //                FechaInicio = dto.FechaInicio,
-        //                FechaFinalizacion = dto.FechaFinalizacion == default(DateTime) ? null : dto.FechaFinalizacion,
-        //                Cargo = dto.Cargo,
-        //                CentroCosto = dto.CentroCosto,
-        //                Salario = dto.Salario
-        //            }
-        //        }
-        //            };
-
-        //            _context.ContratoLaboral.Add(nuevoContrato);
-        //        }
-        //        else
-        //        {
-        //            // ACTUALIZAR CONTRATO EXISTENTE
-
-        //            contratoExistente.EmpresaId = dto.EmpresaId;
-        //            contratoExistente.TipoContratoId = dto.TipoContratoId;
-
-        //            // Buscamos el detalle a actualizar (por Id o tomamos el más reciente)
-        //            var detalleExistente = contratoExistente.ContratoLaboralDetalle
-        //                .FirstOrDefault(d => dto.ContratoLaboralDetalleId > 0 && d.ContratoLaboralDetalleId == dto.ContratoLaboralDetalleId)
-        //                ?? contratoExistente.ContratoLaboralDetalle.OrderByDescending(d => d.FechaInicio).FirstOrDefault();
-
-        //            if (detalleExistente != null)
-        //            {
-        //                // Actualizamos los valores del detalle
-        //                detalleExistente.FechaInicio = dto.FechaInicio;
-        //                detalleExistente.FechaFinalizacion = dto.FechaFinalizacion == default(DateTime) ? null : dto.FechaFinalizacion;
-        //                detalleExistente.Cargo = dto.Cargo;
-        //                detalleExistente.CentroCosto = dto.CentroCosto;
-        //                detalleExistente.Salario = dto.Salario;
-        //            }
-        //            else
-        //            {
-        //                // Si por alguna razón el contrato no tenía detalle en BD, lo agregamos
-        //                contratoExistente.ContratoLaboralDetalle.Add(new ContratoLaboralDetalle
-        //                {
-        //                    FechaInicio = dto.FechaInicio,
-        //                    FechaFinalizacion = dto.FechaFinalizacion == default(DateTime) ? null : dto.FechaFinalizacion,
-        //                    Cargo = dto.Cargo,
-        //                    CentroCosto = dto.CentroCosto,
-        //                    Salario = dto.Salario
-        //                });
-        //            }
-        //    }
-
-        //    // Confirmar los cambios
-        //    return await _context.SaveChangesAsync() > 0;
-        //}
-        //catch (Exception ex)
-        //{
-        //    throw new Exception($"Error al procesar el contrato laboral: {ex.Message}", ex);
-        //}
-    }
-
-
-
-
 
     }
- 
-    
+
+}
+
+
