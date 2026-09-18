db8773a2b747bd4de82a1719bc7d63b61f6f76af
ypirajan19-art <ypirajan19@gmail.com>
2026-09-18 12:36:21 -0500

    Correcion al main de rol service y cargo

--- Miluc.Server/Servicios/Autorizacion/OtpService.cs
+++ Miluc.Server/Servicios/Autorizacion/OtpService.cs
@@ -51,7 +51,7 @@ namespace Miluc.Server.Servicios.Autorizacion
                     true
                 );
             }
-            catch (Exception )
+            catch (Exception ex )
             {
                 throw new Exception($"ERROR EN GENERAR CODIGO DE VERIFICACION,{ex.Message}");
             }
